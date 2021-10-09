6ea9d9651d06c13c3c7cdfa0fd4c96b7803689db
Hectorv72 <hectorvaldezfsa13@gmail.com>
2021-10-09 12:28:04 -0300

    first commit

---
+++ src/components/Album.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+// import ReactDOM from 'react-dom';
+
+export const Album = ({titulo}) => {
+    return (
+        <div>
+            <h5>{titulo}</h5>
+        </div>
+    )
+}
---
+++ src/components/List.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './List.css';
+import { Album } from './Album';
+// import ReactDOM from 'react-dom';
+
+export const List = ({albums}) => {
+
+    const list_music = albums.map( (element,index) => <Album key={ index } titulo={element.musica} /> )
+
+    return (
+        <div className='flex-list'>
+            <h1>Musicas</h1>
+            <div className='list-content'>
+                {list_music}
+            </div>
+        </div>
+    )
+}
